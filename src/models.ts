981b2659fab59efaf56bb5264270ac9a6a9d1689
Isidorio Novaes <ianovaes@gmail.com>
2023-10-25 00:00:44 -0300

    Creating export default

--- src/models.ts
+++ src/models.ts
@@ -5,14 +5,42 @@ export enum accessOptions {
     undefined = 'Não Definido'
   }
 
+  /*
   interface IPerson {
     fullName: string;
     address?: string[];
   }
 
+  interface IPerson {
+    addPerson(): void;
+    updatePerson(): void;
+  }
+
+  
   export interface IUser extends IPerson {
     register?: string | number;
     access?: accessOptions;
     active?: boolean;
   }
+*/
 
+  class Person {
+    // Propriedades
+    address?:string[];
+
+    // Construtor
+    constructor(public fullName: string){
+
+    }
+  }
+
+  export class User extends Person {
+    regidter?: string | number;
+    access?: accessOptions;
+    active?: boolean;
+
+    constructor(public fullName: string,
+      public register?: string | number){
+      super(fullName);
+    }
+  }
